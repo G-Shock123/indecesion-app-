a1ec4d2af88596b4c56a450f5a99922e1cf596c6
Konrad Hylton <konradhylton@Konrads-MacBook-Pro.local>
2019-08-24 16:35:12 -0500

    production build and server setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const publicPath = path.join(__dirname,'..','public')
+const port = process.env.PORT || 3000
 
 app.use(express.static(publicPath))
 
@@ -11,7 +12,7 @@ app.get('*',(req,res)=>{
 
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
 console.log('serve is up');
 
 })
